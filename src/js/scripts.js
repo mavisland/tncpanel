4eae4bd2dcb8f2d9b678adc50bd7dac5b1e7fd91
Tanju Yıldız <ben@tanjuyildiz.com>
2016-11-23 15:25:12 +0300

    added collapsible sidebar & fixed multi-level collapse menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,11 +4,38 @@
   console.log("TNC-Panel is a free admin template / dashboard based on Bootstrap 3.");
 
   // Multi-level collapse menu
-  $('.app-aside .nav .has-child > a').on('click',function(e) {
+  $('.app-aside ul.nav > li:has(ul)').addClass('has-child');
+
+  $('.app-aside ul.nav:not(.nav-sub) > li.has-child > a').click(function(e) {
+    // Disable href action
     e.preventDefault();
-    $(this).next('.nav-sub').slideToggle(300, function() {
-      $(this).parent().toggleClass('active');
-    });
+
+    $('.app-aside ul.nav > li.has-child > ul.nav-sub').slideUp().parent('li').removeClass('active');
+
+    if ($(this).next('.nav-sub').is(':hidden')) {
+      $(this).next('.nav-sub').slideDown(300).parent('li').addClass('active');
+    } else {
+      $(this).next('.nav-sub').slideUp(300).parent('li').removeClass('active');
+    }
+  });
+
+  $('.app-aside ul.nav-sub > li.has-child > a').click(function(e) {
+    // Disable href action
+    e.preventDefault();
+
+    $('.app-aside ul.nav-sub > li.has-child > ul.nav-sub').slideUp().parent('li').removeClass('active');
+
+    if ($(this).next('.nav-sub').is(':hidden')) {
+      $(this).next('.nav-sub').slideDown(300).parent('li').addClass('active');
+    } else {
+      $(this).next('.nav-sub').slideUp(300).parent('li').removeClass('active');
+    }
+  });
+
+  // Sidebar toggle button
+  $('button.aside-toggle').on('click', function() {
+    $(this).toggleClass('active');
+    $('.app').toggleClass('aside-collapse');
   });
 
 })(jQuery);
